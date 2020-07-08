61b72069cd5946615c6d1b028eac3f87f417574c
reenahukum <big.b.it.traininig@gmail.com>
2020-07-08 09:26:07 +0545

    first commit

--- Ex_Files_DBOOPHP/Exercise Files/ch02/02_02/pdo_connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-//$dsn = 'mysql:host=localhost;dbname=oophp';
-//$dsn = 'mysql:host=localhost;dbname=oophp;port=8889';
-$dsn = 'sqlite:C:/xampp/htdocs/oophp/sqlite/oophp.db';
-//$dsn = 'sqlite:/Applications/MAMP/htdocs/oophp/sqlite/oophp.db';
-
-$db = new PDO($dsn, 'oophp', 'lynda');
--- Ex_Files_DBOOPHP/Exercise Files/ch04/04_01/pdo_array.php
+++
@@ -1,9 +0,0 @@
-<?php
-try {
-    require_once '../../includes/pdo_connect.php';
-    $sql = 'SELECT name, meaning FROM names';
-    $result = $db->query($sql);
-    $names = $result->fetchAll();
-} catch (Exception $e) {
-    $error = $e->getMessage();
-}
